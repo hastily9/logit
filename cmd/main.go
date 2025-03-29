945f58f8ad986009365b5deec626a21d5b3c215c
hastily9 <oleksandrkov0602@gmail.com>
2025-03-29 18:59:21 +0200

    Init Go mod and add main.go.

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+)
+
+func run() error {
+	// TODO: Implement logic here
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hastily9/logit
+
+go 1.24.1
